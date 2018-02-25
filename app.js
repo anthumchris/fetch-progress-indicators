8c75299128645935a9de0f40126098065c4272f7
Chris McGowan <chris@anthum.com>
2018-02-24 20:25:57 -0500

    Closes #2 - Better Restarts, Solidified Code
    Restarts no longer require page refresh
    Edge cases tested and prevented
    Code is now "thread-safe" and locks before Promises resolve
    Added Example Description
    Errors now shown

--- app.js
+++ app.js
@@ -1,82 +1,187 @@
-const ProgressBar = (function(){
-  const loading = document.getElementById('loading');
-  const progress = document.getElementById('progress');
-  let started = false, pct;
+'use strict'
 
-  function evalProgress({loaded, total}) {
-    if (!started) {
-      loading.style.opacity=1;
-      started = true;
-    }
-    pct = Math.round(loaded/total*100)+'%';
-    console.log(pct);
-    progress.style.width=pct;
-    if (loaded === total) {
-      console.log('download complete');
-      loading.classList.add('done');
-    }
+class ProgressReportFetcher {
+  constructor(onProgress = function() {}) {
+    this.onProgress = onProgress;
   }
 
-  return {
-    evalProgress
-  }
-})()
+  // mimic native fetch() instantiation and return Promise
+  fetch(input, init = {}) {
+    const request = (input instanceof Request)? input : new Request(input)
+    this._cancelRequested = false;
 
+    return fetch(request, init).then(response => {
+      if (!response.body) {
+        throw Error('ReadableStream is not yet supported in this browser.  <a href="https://developer.mozilla.org/en-US/docs/Web/API/ReadableStream">More Info</a>')
+      }
 
-function setImage(id, url) {
-  const img = document.getElementById(id);
-  img.src=url;
-  img.offsetWidth; // animation enabler
-  img.classList.add('loaded');
-}
+      // this occurs if cancel() was called before server responded (before fetch() Promise resolved)
+      if (this._cancelRequested) {
+        response.body.getReader().cancel();
+        return Promise.reject('cancel requested before server responded.');
+      }
 
-function ProgressResponse(response, onProgress) {
-  if (!response.body) {
-    console.warn("ReadableStream is not yet supported in this browser.  See https://developer.mozilla.org/en-US/docs/Web/API/ReadableStream")
-    return response;
-  }
-  if (!response.ok) {
-    // HTTP error code response
-    return response;
-  }
+      if (!response.ok) {
+        // HTTP error server response
+        throw Error(`Server responded ${response.status} ${response.statusText}`);
+      }
 
-  const contentLength = response.headers.get('content-length');
+      // Server must send CORS header "Access-Control-Expose-Headers: content-length" to access
+      const contentLength = response.headers.get('content-length');
 
-  if (contentLength === null) {
-    // don't track download progress if we can't compare against a total size
-    console.warn('No Content-Length no header in response.  See https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS#Access-Control-Expose-Headers');
-    return response;
-  }
+      if (contentLength === null) {
+        // don't evaluate download progress if we can't compare against a total size
+        throw Error('Content-Length header response header missing.  <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS#Access-Control-Expose-Headers">More Info</a>')
+      }
 
-  let loaded = 0;
-  const total = parseInt(contentLength,10);
-  const reader = response.body.getReader();
-  return new Response(
-    new ReadableStream({
-      start(controller) {
-        return consume();
-        function consume() {
-          return reader.read().then(({done, value}) => {
-            if (done) {
+      const total = parseInt(contentLength,10);
+      let loaded = 0;
+
+      // Set as read-only for stop() to access
+      this._reader=response.body.getReader()
+
+      const me = this;
+
+      return new Response(
+        new ReadableStream({
+          start(controller) {
+            if (me.cancelRequested) {
+              console.log('canceling read')
               controller.close();
               return;
             }
 
-            controller.enqueue(value);
-            loaded += value.byteLength;
-            onProgress.call(null, {loaded, total});
-            return consume();
-          });
-        }
-      }
-    })
-  )
+            read();
+            function read() {
+              me._reader.read().then(({done, value}) => {
+                if (done) {
+                  // ensure onProgress called when content-length=0
+                  if (total === 0) {
+                    me.onProgress.call(me, {loaded, total});
+                  }
+
+                  controller.close();
+                  return;
+                }
+
+                loaded += value.byteLength;
+                me.onProgress.call(me, {loaded, total});
+                controller.enqueue(value);
+                read();
+              });
+            }
+          }
+        })
+      )
+    });
+  }
+
+  cancel() {
+    console.log('download cancel requested.')
+    this._cancelRequested = true;
+    if (this._reader) {
+      console.log('cancelling current download');
+      return this._reader.cancel();
+    }
+    return Promise.resolve();
+  }
 }
 
-console.log('downloading...');
-fetch('https://dev.anthum.com/retain/30kbps/sunrise-progressive.jpg')
-.then(response => new ProgressResponse(response, ProgressBar.evalProgress))
-.then(response => response.blob())
-.then(blob => URL.createObjectURL(blob))
-.then(url => setImage('img', url))
-.catch(error => console.error('error', error));
+
+const imageLoader = (function() {
+  const loader = document.getElementById('loader');
+  const img = loader.querySelector('img');
+  const errorMsg = loader.querySelector('.error');
+  const loading = loader.querySelector('.progress-bar');
+  const progress = loader.querySelector('.progress');
+
+  let locked, started, progressFetcher, pct;
+
+  function downloadDone(url) {
+    console.log('downloadDone()')
+    img.src=url;
+    img.offsetWidth; // pre-animation enabler
+    loader.classList.remove('loading');
+    loader.classList.add('loading-complete');
+    // progressFetcher = null;
+  }
+
+  function startDownload() {
+    // Ensure "promise-safe" (aka "thread-safe" JavaScript).
+    // Caused by slow server response or consequetive calls to startDownload() before stopDownload() Promise resolves
+    if (locked) {
+      console.error('startDownload() failed. Previous download not yet initialized');
+      return;
+    }
+
+    locked = true;
+    stopDownload()
+    .then(function() {
+      locked = false;
+
+      progress.style.transform=`scaleX(0)`;
+      progress.offsetWidth; /* prevent animation when set to zero */
+      started = false;
+      pct = 0;
+
+      loader.classList.add('loading');
+      loader.classList.remove('loading-complete');
+
+
+      if (!progressFetcher) {
+        progressFetcher = new ProgressReportFetcher(updateDownloadProgress);
+      }
+
+      console.log('Starting download...');
+      progressFetcher.fetch('https://dev.anthum.com/retain/30kbps/sunrise-baseline.jpg')
+      .then(response => response.blob())
+      .then(blob => URL.createObjectURL(blob))
+      .then(url => downloadDone(url))
+      .catch(error => showError(error))
+    });
+  }
+
+  function stopDownload() {
+    // stop previous download
+    if (progressFetcher) {
+      return progressFetcher.cancel()
+    } else {
+      // no previous download to cancel
+      return Promise.resolve();
+    }
+  }
+
+  function showError(error) {
+    console.error(error);
+    loader.classList.remove('loading');
+    loader.classList.remove('loading-complete');
+    loader.classList.remove('loading-error');
+    errorMsg.offsetWidth; // pre-animation enabler
+    errorMsg.innerHTML = 'ERROR: '+ error.message;
+    loader.classList.add('loading-error');
+  }
+
+  function updateDownloadProgress({loaded, total}) {
+    if (!started) {
+      loader.classList.add('loading');
+      started = true;
+    }
+    
+    // handle divide-by-zero edge case when Content-Length=0
+    pct = total? loaded/total : 1;
+
+    progress.style.transform=`scaleX(${pct})`;
+    // console.log('downloaded', Math.round(pct*100)+'%')
+    if (loaded === total) {
+      console.log('download complete')
+    }
+  }
+
+  return {
+    startDownload,
+    stopDownload
+  }
+})()
+
+
+imageLoader.startDownload();
